49fc9b899d75fc9cc4728b3bee4070e8df1ba96e
Jack Louis <jbl5925@gmail.com>
2022-03-29 10:33:36 -0500

    Initial Commit

---
+++ Environment/EnvironmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnvironmentApp.swift
+//  Environment
+//
+//  Created by Student on 3/29/22.
+//
+
+import SwiftUI
+
+@main
+struct EnvironmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
